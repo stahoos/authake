f2695355f27e7b70337b9c18fd9ebbce3b0518c4
Ton <github.com@snelweg.net>
2012-01-22 13:47:36 +0100

    fixed getControllers -> http://www.cleverweb.nl/cakephp/list-all-controllers-in-cakephp-2/ Signed-off-by:Ton <github.com@snelweg.net>

--- Controller/Component/AuthakeComponent.php
+++ Controller/Component/AuthakeComponent.php
@@ -1,141 +1,144 @@
 <?php
+
 /*
-    This file is part of Authake.
+  This file is part of Authake.
 
-    Author: Jérôme Combaz (jakecake/velay.greta.fr)
-    Contributors:
+  Author: Jérôme Combaz (jakecake/velay.greta.fr)
+  Contributors:
 
-    Authake is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  Authake is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    Authake is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  Authake is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-*/
+  You should have received a copy of the GNU General Public License
+  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 class AuthakeComponent extends Component {
-  
+
     var $components = array('Session');
-    
     var $_forward = null;
     var $_flashmessage = '';
-    function initialize($controller) { }
+
+    function initialize($controller) {
+        
+    }
+
     function __construct(ComponentCollection $collection, $settings = array()) {
         parent::__construct($collection, $settings);
     }
+
     function startup(&$controller = null) {
         /**
-        * AUTHAKE CONFIGURATION
-        * All these changes can be overrided in AppController->beforeFilter action
-        */
-        
+         * AUTHAKE CONFIGURATION
+         * All these changes can be overrided in AppController->beforeFilter action
+         */
         /**
-        * Base URL, used to insert the application URL in mails.
-        */
-        if(Configure::read('Authake.baseUrl') == null){
+         * Base URL, used to insert the application URL in mails.
+         */
+        if (Configure::read('Authake.baseUrl') == null) {
             Configure::write('Authake.baseUrl', Router::url('/', true));   // set the full application url
         }
-        if(Configure::read('Authake.service') == null){
+        if (Configure::read('Authake.service') == null) {
             Configure::write('Authake.service', 'Authake'); //Name of the service i.e. "Super Authake"
         }
         /**
-        * Default login action
-        */
-        if(Configure::read('Authake.loginAction') == null){
-            Configure::write('Authake.loginAction', array('plugin'=>'authake', 'controller'=>'user', 'action'=>'login'));
+         * Default login action
+         */
+        if (Configure::read('Authake.loginAction') == null) {
+            Configure::write('Authake.loginAction', array('plugin' => 'authake', 'controller' => 'user', 'action' => 'login'));
         }
         /**
          * Used to redirect the users if the current user is logged out. Basically, this
          * is used in case when The login page is the home page. If this is not set to different location, then it's going into recursion.
-        */
-        if(Configure::read('Authake.loggedAction') == null){
-            Configure::write('Authake.loggedAction', array('plugin'=>false, 'controller'=>'entries', 'action'=>'index'));
+         */
+        if (Configure::read('Authake.loggedAction') == null) {
+            Configure::write('Authake.loggedAction', array('plugin' => false, 'controller' => 'entries', 'action' => 'index'));
         }
         /**
-        * Session timeout in seconds, if managed by Authake (or null to disable)
-        */
-        if(Configure::read('Authake.sessionTimeout') == null){
-            Configure::write('Authake.sessionTimeout', 3600*24*7);
+         * Session timeout in seconds, if managed by Authake (or null to disable)
+         */
+        if (Configure::read('Authake.sessionTimeout') == null) {
+            Configure::write('Authake.sessionTimeout', 3600 * 24 * 7);
         }
         /**
-        * Default page when access is denied (should be allowed by ACLs...)
-        */
-        if(Configure::read('Authake.defaultDeniedAction') == null){
-            Configure::write('Authake.defaultDeniedAction', array('plugin'=>'authake', 'controller'=>'user', 'action'=>'denied'));
+         * Default page when access is denied (should be allowed by ACLs...)
+         */
+        if (Configure::read('Authake.defaultDeniedAction') == null) {
+            Configure::write('Authake.defaultDeniedAction', array('plugin' => 'authake', 'controller' => 'user', 'action' => 'denied'));
         }
         /**
-        * Reload all rules every x seconds
-        */
-        if(Configure::read('Authake.rulesCacheTimeout') == null){
+         * Reload all rules every x seconds
+         */
+        if (Configure::read('Authake.rulesCacheTimeout') == null) {
             Configure::write('Authake.rulesCacheTimeout', 300);
         }
         /**
-        * Email which sends the system mails
-        */
-        if(Configure::read('Authake.systemEmail') == null){
+         * Email which sends the system mails
+         */
+        if (Configure::read('Authake.systemEmail') == null) {
             Configure::write('Authake.systemEmail', 'noreply@example.com');
         }
-        if(Configure::read('Authake.systemReplyTo') == null){
+        if (Configure::read('Authake.systemReplyTo') == null) {
             Configure::write('Authake.systemReplyTo', 'noreply@example.com');
         }
         /**
-        * User need to authenticate that he requested the password change
-        * (by receiving the confirmation link at his e-mail)
-        */
-        if(Configure::read('Authake.passwordVerify') == null){
+         * User need to authenticate that he requested the password change
+         * (by receiving the confirmation link at his e-mail)
+         */
+        if (Configure::read('Authake.passwordVerify') == null) {
             Configure::write('Authake.passwordVerify', true);
         }
         /**
-        * Users can register
-        */
-        if(Configure::read('Authake.registration') == null){
+         * Users can register
+         */
+        if (Configure::read('Authake.registration') == null) {
             Configure::write('Authake.registration', true); //or false
         }
         /**
-        * Default group for registered users
-        * If set registered user will be inserted into specified group
-        */
-        if(Configure::read('Authake.defaultGroup') == null){
+         * Default group for registered users
+         * If set registered user will be inserted into specified group
+         */
+        if (Configure::read('Authake.defaultGroup') == null) {
             Configure::write('Authake.defaultGroup', 2); //could be array or single number
         }
         /**
-        * Skip using authake layout for User controller.
-        * This is used to display default layout of the application to actions
-        * like login, register, change password etc.
-        */
-        if(Configure::read('Authake.useDefaultLayout') == null){
+         * Skip using authake layout for User controller.
+         * This is used to display default layout of the application to actions
+         * like login, register, change password etc.
+         */
+        if (Configure::read('Authake.useDefaultLayout') == null) {
             Configure::write('Authake.useDefaultLayout', false); //could be true or false
         }
     }
 
     function beforeFilter(&$controller) { //pr($this);
-        
         //Getting vars
         $this->startup();
-        
+
         // get action path
         // used when the action is called through requestAction()
-        if(!isset($controller->request['url'])){
-            if(isset($controller->request->params['plugin'])){
-                $url = ''.$controller->request->params['plugin'];
+        if (!isset($controller->request['url'])) {
+            if (isset($controller->request->params['plugin'])) {
+                $url = '' . $controller->request->params['plugin'];
             } else {
                 $url = '';
             }
-            $controller->request['url'] = $url.'/'.$controller->request->params['controller'].'/'.$controller->request->params['action'];
+            $controller->request['url'] = $url . '/' . $controller->request->params['controller'] . '/' . $controller->request->params['action'];
         }
         $path = $controller->request['url'];
         if ($path != '/') {
-            $path = '/'.$path;
+            $path = '/' . $path;
         }
         $loginAction = Configure::read('Authake.loginAction');
-        if(is_array(Configure::read('Authake.loginAction'))){
-            $loginAction = "/".implode("/", Configure::read('Authake.loginAction'));
+        if (is_array(Configure::read('Authake.loginAction'))) {
+            $loginAction = "/" . implode("/", Configure::read('Authake.loginAction'));
         }
         if ($path != $loginAction) {
             $this->setPreviousUrl(null);
@@ -145,15 +148,15 @@ class AuthakeComponent extends Component {
         $tm = Configure::read('Authake.sessionTimeout');
         if ($tm && $this->isLogged()) {
             $ts = $this->Session->read('Authake.timestamp');
-            if ((time()-$ts) > $tm) {
+            if ((time() - $ts) > $tm) {
                 $this->setPreviousUrl($path);
                 $this->logout();
                 $this->Session->setFlash(__('Your session expired'), 'warning');
-                $controller->redirect($loginAction);                
-            }            
+                $controller->redirect($loginAction);
+            }
             $this->setTimestamp();
         }
-        
+
         if (!$this->isAllowed($path)) { // check for permissions
             if ($this->isLogged()) { // if denied & logged, write a message
                 if ($this->_flashmessage) { // message from the rule (accept path in %s)
@@ -173,7 +176,7 @@ class AuthakeComponent extends Component {
 
     /**
      * API functions
-    */
+     */
     function setPreviousUrl($url) {
         $this->Session->write('Authake.previousUrl', $url);
     }
@@ -193,11 +196,11 @@ class AuthakeComponent extends Component {
     function getUserId() {
         return $this->Session->read('Authake.id');
     }
-    
+
     function getUserEmail() {
         return $this->Session->read('Authake.email');
     }
-    
+
     function getGroupIds() {
         $gid = $this->Session->read('Authake.group_ids');
         return (empty($gid) ? null : $gid); //If not logged in (or no groups - return null)
@@ -213,13 +216,13 @@ class AuthakeComponent extends Component {
     }
 
     function setTimestamp() {
-            $ts = $this->Session->write('Authake.timestamp', time());
-        }
+        $ts = $this->Session->write('Authake.timestamp', time());
+    }
 
     function login($user) {
-            $this->Session->write('Authake', $user);
-            $this->setTimestamp();
-        }
+        $this->Session->write('Authake', $user);
+        $this->setTimestamp();
+    }
 
     function logout() {
         $this->Session->delete('Authake');
@@ -228,11 +231,11 @@ class AuthakeComponent extends Component {
     function getRules($group_ids = null) {
         $force_reload = (time() - $this->Session->read('Authake.cacheRulesTime')) > Configure::read('Authake.rulesCacheTimeout');
 
-        if($force_reload
-        || is_array($group_ids)
-        //|| ($cacheRules = $this->Session->read('Authake.cacheRules')) === null
-        || $cacheRules = null === null
-            ) {
+        if ($force_reload
+                || is_array($group_ids)
+                //|| ($cacheRules = $this->Session->read('Authake.cacheRules')) === null
+                || $cacheRules = null === null
+        ) {
             App::import("Model", "Authake.Rule");
             $rule = new Rule;
             $cacheRules = $rule->getRules(is_array($group_ids) ? $group_ids : $this->getGroupIds(), true); // use groups provided or take groups of the users
@@ -250,8 +253,8 @@ class AuthakeComponent extends Component {
     function isAllowed($url = "", $group_ids = null) { // $checkStr: "/name/action/" $group_ids: check again thess groups
         $allow = false;
         $rules = $this->getRules($group_ids);
-        foreach( $rules as $data ) {
-            if(preg_match("/^({$data['Rule']['action']})$/i", $url, $matches)) {
+        foreach ($rules as $data) {
+            if (preg_match("/^({$data['Rule']['action']})$/i", $url, $matches)) {
                 $allow = $data['Rule']['permission']; //echo $allow.'=>'.$url.' ** '.$data['Rule']['action'];
                 if ($allow == 'Deny') {
                     $allow = false;
@@ -265,7 +268,6 @@ class AuthakeComponent extends Component {
         return $allow;
     }
 
-
     function getActionsPermissions($group_ids) {
         //pr(getcwd());
 
@@ -273,8 +275,8 @@ class AuthakeComponent extends Component {
         $rules = $this->getRules($group_ids);
         $actionsList = array();
 
-        foreach($controllers as $controller => $actions) {
-            foreach($actions as $k => $action) {
+        foreach ($controllers as $controller => $actions) {
+            foreach ($actions as $k => $action) {
                 $con = strtolower($controller);
                 $permission = $this->_areGroupsAllowed("/{$con}/{$action}/", $rules);
                 $actionsList[$controller][] = array('controller' => $con, 'action' => $action, 'permission' => $permission);
@@ -282,55 +284,52 @@ class AuthakeComponent extends Component {
         }
 
         return $actionsList;
-
     }
 
+    function _getControllers($lowercase = false) {//http://www.cleverweb.nl/cakephp/list-all-controllers-in-cakephp-2/
+        $aCtrlClasses = App::objects('controller');
 
-    function _getControllers($lowercase = false) {
-        $controllerList = array();
-        $controllers = Configure::listObjects('controller');
-        App::import('Controller', $controllers);
-        
-/*  To improve...
-        $controllers[]='User';
-        $controllers[]='Users';
-        $controllers[]='Groups';
-        $controllers[]='Rules';
-        $controllers[]='Denied';
-        App::import('Controller', array('Authake.User', 'Authake.Users', 'Authake.Groups', 'Authake.Rules', 'Authake.Denied'));
-*/
+        foreach ($aCtrlClasses as $controller) {
+            if ($controller != 'AppController') {
+                // Load the controller
+                App::import('Controller', str_replace('Controller', '', $controller));
 
-        foreach($controllers as $controller) {
-            if ($controller != "App") {
-                $className = $controller . 'Controller';
-                $actions = get_class_methods($className);
-                foreach($actions as $k => $v)
-                    if ($v{0} == '_') unset($actions[$k]);
 
+                // Load its methods / actions
+                $aMethods = get_class_methods($controller);
+
+                foreach ($aMethods as $method => $idx) {
+
+                    if ($method{0} == '_') {
+                        unset($aMethods[$idx]);
+                    }
+                }
+
+                // Load the ApplicationController (if there is one)
+                App::import('Controller', 'AppController');
                 $parentActions = get_class_methods('AppController');
-                if ($lowercase) $controller = strtolower($controller);
-                $controllersList[$controller] = array_diff($actions, $parentActions);
+
+                $controllers[$controller] = array_diff($aMethods, $parentActions);
             }
         }
-        
-        return $controllersList;
+        return $controllers;
     }
-
-
 
     // Function to check the access for the controller / action
     function _areGroupsAllowed($url = "", $rules) { // $checkStr: "/name/action/" $group_ids: check again thess groups
         $allow = false;
-        foreach( $rules as $data ) {
-        if(preg_match("/{$data['Rule']['action']}/i", $url, $matches)) {
-            $allow = $data['Rule']['permission'];
-            if ($allow == 'Deny')
-                $allow = false;
-            else
-                $allow = true;
+        foreach ($rules as $data) {
+            if (preg_match("/{$data['Rule']['action']}/i", $url, $matches)) {
+                $allow = $data['Rule']['permission'];
+                if ($allow == 'Deny')
+                    $allow = false;
+                else
+                    $allow = true;
             }
         }
         return $allow;
     }
+
 }
+
 ?>
